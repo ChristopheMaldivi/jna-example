ea9eb71ffb03c4c17b22332798e9ebed5cee19a1
Christophe Maldivi <christophe.maldivi@orange.com>
2020-11-19 11:46:44 +0100

    on java side, call the native function; test is red and it is fine

--- src/main/java/org/tof/example/DoTheJob.java
+++ src/main/java/org/tof/example/DoTheJob.java
@@ -1,7 +1,21 @@
 package org.tof.example;
 
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
 public class DoTheJob {
+
+    public interface NativeLibraryInterface extends Library {
+        // library name:
+        // - linux: 'libdothejob.so'
+        // - mac: 'libdothejob.dylib'
+        NativeLibraryInterface nativeLib = Native.load("dothejob", NativeLibraryInterface.class);
+
+        String doTheJobNative();
+    }
+
+
     String doTheJob() {
-        return "it works";
+        return NativeLibraryInterface.nativeLib.doTheJobNative();
     }
 }
