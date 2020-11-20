c963aac5c01c81c6bbc0935a6637372c59649827
Christophe Maldivi <christophe.maldivi@orange.com>
2020-11-20 10:07:50 +0100

    fix build native library on linux

--- native-library/build-macos.sh
+++ native-library/build-macos.sh
@@ -2,4 +2,5 @@
 rm -f *.o *.dylib
 gcc -c dothejob.c | exit 1
 gcc -dynamiclib dothejob.o -o libdothejob.dylib | exit 2
-cp libdothejob.dylib ../src/main/resources
+cp libdothejob.dylib ../src/main/resources | exit 3
+rm -f *.o *.dylib
