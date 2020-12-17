018710e3453e018976ba8dcd0083a01c2facd4e9
Denis Boisset <denis.boisset@orange.com>
2020-12-17 15:38:29 +0100

    add ByteBuffer

--- src/main/java/org/tof/example/LibDivsufsortNative.java
+++ src/main/java/org/tof/example/LibDivsufsortNative.java
@@ -2,10 +2,15 @@ package org.tof.example;
 
 import com.sun.jna.Native;
 
+import java.nio.ByteBuffer;
+
 public class LibDivsufsortNative {
     static {
         Native.register("divsufsort");
     }
 
     public static native int divsufsort(byte[] inputArray, int[] outputArray, int arraySize);
+
+    public static native int divsufsort(ByteBuffer inputByteBuffer, ByteBuffer outputByteBuffer, int arraySize);
+
 }
--- src/test/java/org/tof/example/LibDivsufsortTest.java
+++ src/test/java/org/tof/example/LibDivsufsortTest.java
@@ -3,6 +3,9 @@ package org.tof.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.nio.ByteBuffer;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.BDDAssertions.then;
 
 public class LibDivsufsortTest {
@@ -21,5 +24,25 @@ public class LibDivsufsortTest {
         // then
         then(index).isEqualTo(0);
         then(outputArray).isEqualTo(new int[]{10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2});
+    }
+
+    @Test
+    @DisplayName("compute suffix array indices with native library and Byte Buffer")
+    void compute_suffix_array_indices_with_native_library_and_Byte_Buffer() {
+        // given
+        int arraySize = 11;
+        ByteBuffer inputByteBuffer = ByteBuffer.allocateDirect(arraySize);
+        inputByteBuffer.put("abracadabra".getBytes());
+        ByteBuffer outputByteBuffer = ByteBuffer.allocateDirect(arraySize * Integer.BYTES);
+
+        // when
+        int index = LibDivsufsortNative.divsufsort(inputByteBuffer, outputByteBuffer, arraySize);
+
+        // then
+        then(index).isEqualTo(0);
+        int[] outputCopyOnJavaHeap = new int[arraySize];
+        outputByteBuffer.asIntBuffer().get(outputCopyOnJavaHeap);
+        IntStream.range(0, outputCopyOnJavaHeap.length).forEach(i -> outputCopyOnJavaHeap[i] = Integer.reverseBytes(outputCopyOnJavaHeap[i]));
+        then(outputCopyOnJavaHeap).isEqualTo(new int[]{10, 7, 0, 3, 5, 8, 1, 4, 6, 9, 2});
     }
 }
